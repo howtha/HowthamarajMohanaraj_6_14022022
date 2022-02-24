bcc446c15399099c0283ee9ff9631daa08ae9624
Howtha MOHANARAJ <howtha.mohanaraj@gmail.com>
2022-02-24 21:33:38 +0100

    css header

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,32 +1,24 @@
     async function getPhotographers() {
         fetch("./data/photographers.json")
-        .then(response => 
-            {
-                return response.json();
-            })
+        .then(response => response.json())
         .then(jsondata => 
+            {   
+                for (cpt = 0; cpt <= jsondata.photographers.length; cpt++)
+                {
+                    console.log(jsondata.photographers[cpt].name);
+                }
+        
+        const photographers = 
             {
-                console.log(jsondata.photographers);
-                console.log(jsondata.photographers.length);
-
-                const photographers = [
-                    {
-                        for (let cpt = 0; cpt <= jsondata.photographers.length; cpt++)
-                        {
-                            "name": "Ma data test", //jsondata.photographers[0].name
-                            "id": 1,
-                            "city": "Paris",
-                            "country": "France",
-                            "tagline": "Ceci est ma data test",
-                            "price": 400,
-                            "portrait": "account.png"
-                        }
-                    }
-                ]
-
-            });
-        
-        
+                "name": "Data",
+                "id": 1,
+                "city": "Paris",
+                "country": "France",
+                "tagline": "Ceci est ma data test",
+                "price": 400,
+                "portrait": "account.png"
+            }
+        });
         // et bien retourner le tableau photographers seulement une fois
         return ({
             photographers: [...photographers, ...photographers, ...photographers]})
