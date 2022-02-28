b44b2fa695a2e5bc7b1256143076c52b6a6fa36f
Howtha MOHANARAJ <howtha.mohanaraj@gmail.com>
2022-02-28 23:52:29 +0100

    object photographers ENFIIIIN !!! ;) merci Thibault

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,44 +1,44 @@
-    async function getPhotographers() {
-        fetch("./data/photographers.json")
-        .then(response => response.json())
-        .then(jsondata => 
-            {   
-                for (cpt = 0; cpt <= jsondata.photographers.length; cpt++)
-                {
-                    console.log(jsondata.photographers[cpt].name);
-                }
-        
-        const photographers = 
+async function getPhotographers() {
+    fetch("./data/photographers.json")
+    .then(response => response.json())
+    .then(jsondata => 
+        {   
+            for (cpt = 0; cpt <= jsondata.photographers.length; cpt++)
             {
-                "name": "Data",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
+                //console.log(jsondata.photographers[cpt].name);
+                const photographers = 
+                    {
+                        "name": jsondata.photographers[cpt].name,
+                        "id": jsondata.photographers[cpt].id,
+                        "city": jsondata.photographers[cpt].city,
+                        "country": jsondata.photographers[cpt].country,
+                        "tagline": jsondata.photographers[cpt].tagline,
+                        "price": jsondata.photographers[cpt].price,
+                        "portrait": jsondata.photographers[cpt].portrait 
+                    }
+                console.log(photographers);
             }
-        });
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
-
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
-
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
-
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
+            return ({
+                photographers: [...photographers, ...photographers]})
+        })
+    .catch(err => console.log("Erreur" + err))
     
-    init();
-    
+}
+
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
